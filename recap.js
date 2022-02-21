85cd55140cf522f3b70bf217eb92e7be73cc3a20
mdtouhidulislam136 <mdtouhidul.islam136@gmail.com>
2022-02-21 19:52:18 +0200

    Initial commit

---
+++ recap.js
@@ -0,0 +1,11 @@
+// const will never change, better to use let
+
+// tamplate string
+
+const mynotes = `Hi dear someone, I am going to visit your home after next week, I really appriciate your work for the community`;
+
+// default parameter
+
+
+
+console.log(mynotes);
