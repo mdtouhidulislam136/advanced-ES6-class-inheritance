4f2887b1294d874b94a9c31823b28b75b8e2a502
mdtouhidulislam136 <mdtouhidul.islam136@gmail.com>
2022-02-22 20:42:00 +0200

    filter and find of JavaScript ES6

--- filter-find.js
+++ filter-find.js
@@ -3,4 +3,18 @@ const numbers = [5, 35, 43, 43, 544, 34, 89, 4, 33, 43, 34, 13, ,33, 3];
 const BigNumbers = numbers.filter(number => number> 20);
 const smallNumbers = numbers.filter(number => number < 10);
 
-console.log(BigNumbers, smallNumbers);
+
+const products = [
+    {name: 'Water bottle', price: 50, color: 'black'},
+    {name: 'Mobile phone', price: 500, color: 'aqua'},
+    {name: 'Smart watch', price: 500, color: 'gold'},
+    {name: 'Node', price: 30, color: 'blue'},
+    {name: 'Waterglass', price: 30, color: 'white'},
+]
+
+const expensive = products. filter(product => product.price > 100);
+const black = products. filter(product => product.color == 'hh');
+
+const blueItem = products.find(product => product.color == 'blue' )
+
+console.log(BigNumbers, smallNumbers, expensive, black, blueItem);
